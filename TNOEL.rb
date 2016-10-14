20bcc5f076a9038338af01323316d7498d8a8463
Hanterss <dr.sheshenya@gmail.com>
2016-10-14 12:38:51 +0300

    rabochaya proga

---
+++ TNOEL.rb
@@ -0,0 +1,82 @@
+print "Введите строку "
+s1 = gets
+
+#s1 = "abracadabra"
+a = s1.count("a")
+b = s1.count("b")
+c = s1.count("c")
+d = s1.count("d")
+e = s1.count("e")
+f = s1.count("f")
+g = s1.count("g")
+h = s1.count("h")
+i = s1.count("i")
+j = s1.count("j")
+k = s1.count("k")
+l = s1.count("l")
+m = s1.count("m")
+n = s1.count("n")
+o = s1.count("o")
+p = s1.count("p")
+q = s1.count("q")
+r = s1.count("r")
+s = s1.count("s")
+t = s1.count("t")
+u = s1.count("u")
+v = s1.count("v")
+w = s1.count("w")
+x = s1.count("x")
+y = s1.count("y")
+z = s1.count("z")
+puts "a" 
+puts a
+puts "b" 
+puts b
+puts "c" 
+puts c
+puts "d" 
+puts d
+puts "e" 
+puts e
+puts "f" 
+puts f
+puts "g"
+puts g
+puts "h" 
+puts h
+puts "i" 
+puts i
+puts "j" 
+puts j
+puts "k" 
+puts k
+puts "l" 
+puts l
+puts "m" 
+puts m
+puts "n" 
+puts n
+puts "o" 
+puts o
+puts "p" 
+puts p
+puts "q" 
+puts q
+puts "r" 
+puts r
+puts "s" 
+puts s
+puts "t" 
+puts t
+puts "u" 
+puts u
+puts "v" 
+puts v
+puts "w" 
+puts w 
+puts "x" 
+puts x
+puts "y" 
+puts y 
+puts "z" 
+puts z
